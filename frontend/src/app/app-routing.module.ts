54dd5efefe74967b8b4f90ed079e57c9f88dc0f7
jb-abhi <jbabhi2@gmail.com>
2022-07-31 00:08:14 +0530

    Login completed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
