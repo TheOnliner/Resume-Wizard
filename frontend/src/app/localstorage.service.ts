9255ea26406dc2ba671d731087b78654b3ef6c65
jb-abhi <jbabhi2@gmail.com>
2022-08-01 12:51:52 +0530

    New changes

---
+++ frontend/src/app/localstorage.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+const TOKEN = 'jwtToken';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalstorageService {
+  constructor() {}
+
+  setToken(data: any) {
+    localStorage.setItem(TOKEN, data);
+  }
+  getToken(): string {
+    return JSON.parse(localStorage.getItem(TOKEN)!);
+  }
+  removeToken() {
+    localStorage.removeItem(TOKEN);
+  }
+}
