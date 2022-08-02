b65de2ca57bf74e9301d5f97175e3229b8a74101
jb-abhi <jbabhi2@gmail.com>
2022-08-02 17:04:10 +0530

    userprofile ongoing

--- frontend/src/app/admin/pages/userprofiles/userforms/userforms.component.ts
+++ frontend/src/app/admin/pages/userprofiles/userforms/userforms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-userforms',
@@ -8,19 +8,32 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class UserformsComponent implements OnInit {
   form: FormGroup;
+  isSubmitted:boolean= false;
 
   constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      username:[''],
-      email:[''],
-      password:[''],
-      isadmin:['']
+      username:['',Validators.required],
+      email:['',[Validators.email,Validators.required]],
+      password:['',Validators.required],
+      isadmin:[false, Validators.requiredTrue]
     })
   }
 
   onSubmit(){
-    console.log('I am button')
+    this.isSubmitted =true;
+    if(this.form.invalid){
+      return;    
+    }
+
+    console.log(this.form.controls?.['username'].value);
+    console.log(this.form.controls?.['email'].value);
+    console.log(this.form.controls?.['password'].value);
+    console.log(this.form.controls?.['isadmin'].value);
+  }
+
+  get userForm(){
+    return this.form.controls;
   }
 }
