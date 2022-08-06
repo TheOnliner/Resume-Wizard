af0cceabd01edc1240bdd395237e9133cbeb465c
LIJIKN <102722441+LIJIKN@users.noreply.github.com>
2022-08-06 19:24:44 +0530

    education

---
+++ frontend/src/app/dashboard/templatemodel/templatemodel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templatemodel',
+  templateUrl: './templatemodel.component.html',
+  styleUrls: ['./templatemodel.component.scss']
+})
+export class TemplatemodelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
