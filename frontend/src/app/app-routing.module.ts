a36ece897f578fe03c56d811655d12461ddb8a68
jb-abhi <jbabhi2@gmail.com>
2022-08-06 22:01:37 +0530

    dashboard working

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,10 +5,13 @@ import { AdminhomeComponent } from './admin/pages/adminhome/adminhome.component'
 import { UserformsComponent } from './admin/pages/userprofiles/userforms/userforms.component';
 import { UserprofilesComponent } from './admin/pages/userprofiles/userprofiles.component';
 import { AuthGuard } from './auth-guard.service';
+import { CareerObjectiveComponent } from './dashboard/career-objective/career-objective.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EducationComponent } from './dashboard/education/education.component';
 import { ResumecontactComponent } from './dashboard/resumecontact/resumecontact.component';
+import { SummaryComponent } from './dashboard/summary/summary.component';
 import { Template1Component } from './dashboard/template1/template1.component';
+import { Template3Component } from './dashboard/template3/template3.component';
 import { TemplatemodelComponent } from './dashboard/templatemodel/templatemodel.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
@@ -18,8 +21,18 @@ const routes: Routes = [
     path: '',
     canActivate:[],//AuthGuard
     children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'admin', component: AdminComponent,
+      { path: 'dashboard', component: DashboardComponent,
+      children:[
+        { path: 'education', component: EducationComponent},
+        { path: 'career-objective', component: CareerObjectiveComponent},
+        { path: 'contact', component: ResumecontactComponent},
+        { path: 'summary', component: SummaryComponent},
+        { path: 'templates', component: TemplatemodelComponent,children:[
+          { path: '1', component: Template1Component},
+          { path: '3', component: Template3Component},
+        ]}
+      ] },
+      { path: 'admin', component: AdminComponent, 
     children:[
       {path: 'home', component: AdminhomeComponent},
       {path: 'userprofiles', component : UserprofilesComponent},
@@ -28,10 +41,7 @@ const routes: Routes = [
     ] },
     ],
   },
-  { path: 'education', component: EducationComponent},
-  { path: 'resumecontact', component: ResumecontactComponent},
-  { path: 'template1', component: Template1Component},
-  { path: 'templatemodel', component: TemplatemodelComponent}
+ 
 ];
 
 @NgModule({
