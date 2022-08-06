353e657eacdab7f4e3dae96688302cbb0bb2ab92
jb-abhi <jbabhi2@gmail.com>
2022-08-07 00:17:43 +0530

    Resume building

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,33 +5,29 @@ import { AdminhomeComponent } from './admin/pages/adminhome/adminhome.component'
 import { UserformsComponent } from './admin/pages/userprofiles/userforms/userforms.component';
 import { UserprofilesComponent } from './admin/pages/userprofiles/userprofiles.component';
 import { AuthGuard } from './auth-guard.service';
-import { CareerObjectiveComponent } from './dashboard/career-objective/career-objective.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { EducationComponent } from './dashboard/education/education.component';
-import { ResumecontactComponent } from './dashboard/resumecontact/resumecontact.component';
-import { SummaryComponent } from './dashboard/summary/summary.component';
-import { Template1Component } from './dashboard/template1/template1.component';
-import { Template3Component } from './dashboard/template3/template3.component';
-import { TemplatemodelComponent } from './dashboard/templatemodel/templatemodel.component';
+import { CareerObjectiveComponent } from './dashboard/forms/career-objective/career-objective.component';
+import { DashboardComponent } from './dashboard/dashboardhome/dashboard.component';
+import { EducationComponent } from './dashboard/forms/education/education.component';
+import { ResumecontactComponent } from './dashboard/forms/resumecontact/resumecontact.component';
+import { SummaryComponent } from './dashboard/forms/summary/summary.component';
+import { Template1Component } from './dashboard/templates/template1/template1.component';
+import { Template3Component } from './dashboard/templates/template3/template3.component';
+import { TemplatemodelComponent } from './dashboard/forms/templatemodel/templatemodel.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   {
     path: '',
-    canActivate:[],//AuthGuard
     children: [
-      { path: 'dashboard', component: DashboardComponent,
-      children:[
-        { path: 'education', component: EducationComponent},
-        { path: 'career-objective', component: CareerObjectiveComponent},
-        { path: 'contact', component: ResumecontactComponent},
-        { path: 'summary', component: SummaryComponent},
-        { path: 'templates', component: TemplatemodelComponent,children:[
-          { path: '1', component: Template1Component},
-          { path: '3', component: Template3Component},
-        ]}
-      ] },
+      { path: 'dashboard', component: DashboardComponent},
+      { path: 'dashboard/form-education', component: EducationComponent},
+      { path: 'dashboard/form-career-objective', component: CareerObjectiveComponent},
+      { path: 'dashboard/form-contact', component: ResumecontactComponent},
+      { path: 'dashboard/form-summary', component: SummaryComponent},
+      { path: 'dashboard/templates', component: TemplatemodelComponent},
+      { path: 'dashboard/templates/1', component: Template1Component},
+      { path: 'dashboard/templates/3', component: Template3Component},  
       { path: 'admin', component: AdminComponent, 
     children:[
       {path: 'home', component: AdminhomeComponent},
@@ -39,9 +35,8 @@ const routes: Routes = [
       {path: 'userprofiles/forms', component : UserformsComponent},
       {path: 'userprofiles/forms/:id', component : UserformsComponent}
     ] },
-    ],
-  },
- 
+    ]
+  }
 ];
 
 @NgModule({
