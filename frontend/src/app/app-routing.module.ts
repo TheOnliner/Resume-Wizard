b9e6bcbbe5aa1b2756993fdc050e81023722b89d
LIJIKN <102722441+LIJIKN@users.noreply.github.com>
2022-08-07 13:59:08 +0530

    changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { CareerObjectiveComponent } from './dashboard/career-objective/career-objective.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EducationComponent } from './dashboard/education/education.component';
 import { ResumecontactComponent } from './dashboard/resumecontact/resumecontact.component';
+import { SummaryComponent } from './dashboard/summary/summary.component';
 import { Template1Component } from './dashboard/template1/template1.component';
+import { Template3Component } from './dashboard/template3/template3.component';
 import { TemplatemodelComponent } from './dashboard/templatemodel/templatemodel.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
@@ -18,14 +21,12 @@ const routes: Routes = [
   {
     path: '',
     children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'admin', component: AdminComponent },
+      { path:'dashboard', component: DashboardComponent, },
+        
+      { path:'admin', component: AdminComponent },
     ],
   },
-  { path: 'education', component: EducationComponent},
-  { path: 'resumecontact', component: ResumecontactComponent},
-  { path: 'template1', component: Template1Component},
-  { path: 'templatemodel', component: TemplatemodelComponent}
+   
 ];
 
 @NgModule({
